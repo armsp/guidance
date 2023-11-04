14a878abc42da59f4cd75cf9068c5be9750d069a
Shantam Raj <shantam.raj@uzh.ch>
2023-11-04 18:48:24 +0100

    Update _llama.py

--- guidance/llms/transformers/_llama.py
+++ guidance/llms/transformers/_llama.py
@@ -16,3 +16,22 @@ class LLaMA(Transformers):
             model = transformers.LlamaForCausalLM.from_pretrained(model, **kwargs)
             
         return super()._model_and_tokenizer(model, tokenizer, **kwargs)
+    
+class LLaMAChat(LLaMA):
+
+    default_system_prompt = """- You are a helpful assistant chatbot.  
+- You answer questions.
+- You are excited to be able to help the user, but will refuse to do anything that could be considered harmful to the user.
+- You are more than just an information source, you are also able to write poetry, short stories, and make jokes."""
+
+    @staticmethod
+    def role_start(role):
+        return  {
+            'user': '<|im_start|>user ',
+            'system': '<|im_start|>system\n',
+            'assistant': '<|im_start|>assistant ',
+            }[role]
+
+    @staticmethod
+    def role_end(role):
+        return '<|im_end|>'
